c6515e390ad082487b4655225ce8557601636c0a
John Powers <john.powers070@gmail.com>
2018-05-23 11:59:54 -0400

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# ticker animation basics using GreenSock's TimelineMax
+
+
+### a timeline is used with callbacks for onStart and onComplete to 
+### move DOM object from transparent to apparent and back to transparent
+### movement of DOM object is left to right and back (as transparent)
---
+++ script.js
@@ -0,0 +1,58 @@
+
+function end(element){
+  
+  TweenLite.to(element,0.1,{
+  	                      backgroundColor:'transparent',
+  	                      color:'transparent',
+  	                      x:-500,
+                          });
+  
+}
+function start(element){
+  
+  TweenLite.to(element,0.5,{
+  	                        color:'black',
+                           });
+  
+}
+
+window.onload = function(){
+
+    var arr = ["FB","AAPL","AMZN"];
+
+	var t1 = new TimelineMax();
+
+	var ob1 = document.getElementById("one");
+	var ob2 = document.getElementById("two");
+	var ob3 = document.getElementById("three");
+
+    ob1.innerHTML = arr[0];
+    ob2.innerHTML = arr[1];
+    ob3.innerHTML = arr[2];
+
+	t1.to(ob1,2,{x:500,
+       	          y:0,
+       	          onStart:start,
+       	          onStartParams:[ob1],
+       	          onComplete:end,
+       	          onCompleteParams:[ob1]
+       	          })
+	   .to(ob2,2,{x:500,
+       	          y:0,
+       	          onStart:start,
+       	          onStartParams:[ob2],
+       	          color:'transparent',
+       	          onComplete:end,
+       	          onCompleteParams:[ob2]
+       	          },1.0)
+       .to(ob3,2,{x:500,
+       	          y:0,
+       	          onStart:start,
+       	          onStartParams:[ob3],
+       	          color:'transparent',
+       	          onComplete:end,
+       	          onCompleteParams:[ob3]
+       	          },2.0)
+       count++;    
+       	                                                      
+}
